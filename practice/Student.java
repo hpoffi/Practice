2c4df4e8b51d6a0c7f1835274756e57b16dbc5f0
hpoffi <harshitpatel7909@gmail.com>
2024-02-22 04:43:20 +0530

    Pre-final commit

--- practice/Student.java
+++ practice/Student.java
@@ -19,18 +19,32 @@ public class Student {
 
     public int getTotalSumOfMarks() {
         int sum = 0;
-        for(int marks : marks){
-            sum+= marks;
+        for(int mark : marks){
+            sum+= mark;
         }
         return sum;
     }
 
     public int getMaximumMarks() {
-        
-        
+        int maxmarks = 0;
+        for(int mark : marks){
+            if(mark > maxmarks){
+                maxmarks = mark;
+            }
+        }
+        return maxmarks;
+
+          
     }
 
     public int getMinimumMarks() {
+        int minimarks =100;
+        for(int mark : marks){
+            if(mark < minimarks){
+                minimarks = mark;
+            }
+        }
+        return minimarks;
         
     }
 
--- practice/StudentRunner.java
+++ practice/StudentRunner.java
@@ -1,15 +1,22 @@
 package practice;
 
 public class StudentRunner {
-    public static void main(String[] args){
-        int[] marks = {99,98,97,96,95};
-        Student student = new Student("Ranga",marks);
+    public static void main(String[] args) {
+        int[] marks = { 99, 98, 97, 96, 95 };
+
+        Student student = new Student("Ranga", marks);
+
         int number = student.getNumberOfMarks();
         System.out.println("Numberof marks: " + number);
+
         int sum = student.getTotalSumOfMarks();
         System.out.println("getTotalSumOfMarks: " + sum);
-        int maximumMark = student.getMaximumMarks();
-        int minimumMark = student.getMinimumMarks();
+
+        int maxmarks = student.getMaximumMarks();
+        System.out.println("MaximumMarks: " + maxmarks);
+
+        int minimarks = student.getMinimumMarks();
+        System.out.println("MinimumMarks: " + minimarks);
     }
-    
+
 }
